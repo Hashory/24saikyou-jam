36340160bd1bf30ada897bcabdab229aa9798877
Hashory <89839285+Hashory@users.noreply.github.com>
2024-04-28 22:47:57 +0900

    GameOverのシーンの追加
    スペースキーを押すと、シーンを推移するように、変更できるようなコードが入ってます。

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("To TItle");
+            // タイトルシーンに変更する
+            // SceneManager.LoadScene("TitleScene"); 
+        }
+    }
+}
