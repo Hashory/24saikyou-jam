fd8ccf504b125aaf87bef5c04d47c405a88ce6e6
SETSUNA-0601 <t1208t0601t@gmail.com>
2024-04-29 08:41:29 +0900

    debug mode

--- Assets/Scripts/Spowner.cs
+++ Assets/Scripts/Spowner.cs
@@ -119,8 +119,8 @@ public class Spowner : MonoBehaviour
 
             default:
                 Debug.Log("不正な参照");
-                currentspawninterval = 0.1f;
-                Police.speed = 10.0f;
+                currentspawninterval = 0.05f;
+                Police.speed = 15.0f;
                 break;
         }
     }
