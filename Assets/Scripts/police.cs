12a2a93e5540bd3773a2c3e1744ac89bed806df4
SETSUNA-0601 <t1208t0601t@gmail.com>
2024-04-28 15:01:38 +0900

    r

--- Assets/Scripts/police.cs
+++ Assets/Scripts/police.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class police : MonoBehaviour
+public class Police : MonoBehaviour
 {
     public Transform potion; // potionオブジェクトへの参照
     public float speed = 1.0f; // 移動速度
     public int HP = 1; // HP
     private float startY; // 初期のY座標
+    public Potion_manager scoreManager; // PotionManagerへの参照
 
     void Start()
     {
+        // potionタグを持つオブジェクトを見つける
         if (potion == null)
         {
-            potion = GameObject.FindGameObjectWithTag("potion").transform; // potionタグを使用してオブジェクトを見つける
+            potion = GameObject.FindGameObjectWithTag("potion").transform;
         }
         startY = transform.position.y; // 初期のY座標を保存
 
@@ -25,11 +28,17 @@ public class police : MonoBehaviour
 
         // 初期位置のY座標を固定する
         transform.position = new Vector3(transform.position.x, startY, transform.position.z);
+
+        // potionオブジェクトからPotionManagerコンポーネントを取得
+        if (scoreManager == null)
+        {
+            scoreManager = FindObjectOfType<Potion_manager>();
+        }
     }
 
     void Update()
     {
-        // 敵をpotionの方向に移動させる。Y座標は変更しない。
+        // potionの方向に移動させる
         Vector3 moveDirection = potion.position - transform.position;
         moveDirection.y = 0; // Y軸の変動を0にする
         moveDirection.Normalize(); // ベクトルの正規化
@@ -46,12 +55,20 @@ public class police : MonoBehaviour
             if (HP <= 0)
             {
                 Debug.Log("破壊");
+                if (scoreManager != null)
+                {
+                    scoreManager.IncreaseScore();  // スコアを増やす
+                }
                 Destroy(gameObject);
             }
         }
         else if (other.gameObject.CompareTag("potion"))
         {
             Debug.Log("到達");
+            if (scoreManager != null)
+            {
+                scoreManager.DecreaseHP();  // 体力を減らす
+            }
             Destroy(gameObject);
         }
     }
